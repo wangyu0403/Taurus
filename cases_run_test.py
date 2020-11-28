50e38bbdbc6cef8b572fbeac226a6c7474f0e8f4
王昱 <wyhuyawangyu@qq.com>
2020-11-28 19:07:47 +0800

    全他妈是对的

--- cases_run_test.py
+++ cases_run_test.py
@@ -10,12 +10,12 @@ def test_case_001():
 
 def test_case_002():
     print("this is test_case_002")
-    assert 1 in [2, 3, 4]
+    assert 1 in [1, 2, 3, 4]
 
 
 def test_case_003():
     print("this is test_case_003")
-    assert 1 >= 3
+    assert 11 >= 3
 
 
 def test_case_004():
@@ -25,7 +25,7 @@ def test_case_004():
 
 def test_case_005():
     print("this is test_case_005")
-    assert 1 < 1
+    assert 1 == 1
 
 
 # def func(x):
